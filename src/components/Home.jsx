4a19572122342822b7091be46da48c6437107055
forLoop94 <charlesuwem94@gmail.com>
2024-01-30 11:27:34 +0100

    Add Vision and mission component to landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,20 @@
 import "../css/home.css";
 import { Footer } from "./Footer";
 import { About } from "./About";
+import { VisionMission } from "./VisionMission";
 import Slideshow from "./Slideshow";
 
 export const Home = () => {
   return (
-    // <div className="headline vh-100 vw-100 text-white d-flex justify-content-center flex-column align-items-center">
-    //   <h1>Island Construction Co</h1>
-    //   <h5>Making dreams come true</h5>
-    // </div>
-    <section>
-      <Slideshow />
-      <About />
-      <Footer/>
+    <section className="homepage-wrapper">
+      <Slideshow className="home-slides" />
+      <div className="vision-mission-wrapper">
+        <VisionMission className="vision-mission" />
+      </div>
+      <div className="about-container">
+        <About className="home-about" />
+      </div>
+      <Footer />
     </section>
   );
 };
