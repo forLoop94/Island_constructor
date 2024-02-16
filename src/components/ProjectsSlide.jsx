8bcfeb841e864db0e3cfe43970b74a958d08a76c
forLoop94 <charlesuwem94@gmail.com>
2024-02-16 13:20:30 +0100

    Style details button in main projects page

--- src/components/ProjectsSlide.jsx
+++ src/components/ProjectsSlide.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import "../css/Slideshow.css";
-import { projectsData } from "../data.js"
+import { projectsData } from "../data.js";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
 const ProjectsSlide = () => {
   const [slideIndex, setSlideIndex] = useState(1);
@@ -12,7 +13,7 @@ const ProjectsSlide = () => {
     showSlides(slideIndex);
     const intervalId = setInterval(() => {
       plusSlides(1);
-    }, 10000);
+    }, 5000);
 
     return () => clearInterval(intervalId);
   }, [slideIndex]);
@@ -56,15 +57,22 @@ const ProjectsSlide = () => {
               <h1 className="text-white">{data["details header"]}</h1>
               <p className="text-white">{data["details body"]}</p>
             </div>
-            <button className="btn btn-primary" onClick={() => navigate(`/projects/${index}`)}>details</button>
+            <div className="pro-btn-wrapper">
+              <button
+                className="btn-dark-bg"
+                onClick={() => navigate(`/projects/${index}`)}
+              >
+                See details
+              </button>
+            </div>
           </div>
         ))}
       </div>
       <a className="prev" onClick={() => plusSlides(-1)}>
-        &#10094;
+        <BsChevronLeft />
       </a>
       <a className="next" onClick={() => plusSlides(1)}>
-        &#10095;
+          <BsChevronRight />
       </a>
       <div className="slide-indicator">
         {projectsData.map((_, index) => (
