b80b090911fc0978b22bb6b3f2bddd6679e03cab
forLoop94 <charlesuwem94@gmail.com>
2024-02-20 11:20:30 +0100

    Fix alignment problems in project overview

--- src/components/ProjectsOverview.jsx
+++ src/components/ProjectsOverview.jsx
@@ -10,20 +10,20 @@ const ProjectsOverview = () => {
   return (
     <section>
       <div className="pro-details-container">
-        <h1>PROJECTS OVERVIEW</h1>
-        <p>
+        <h1 className="news-header primary-text">PROJECTS OVERVIEW</h1>
+        <p className="primary-text">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure a
           praesentium distinctio dolor ducimus delectus eius, at nobis natus
           earum. Enim aliquam rerum hic repellat aut dolore, odio est adipisci
           tempore placeat maiores tempora, optio quo sint, numquam magni
           reprehenderit?
         </p>
-        <h3>All Projects</h3>
+        <h3 className="news-header primary-text">All Projects</h3>
         <div className="grid-container">
           {projectsData.map((project, index) => (
             <div className="grid-item" key={index}>
               <img
-                className="w-100"
+                className="overview-image"
                 src={project.image}
                 alt={project["details header"]}
               />
